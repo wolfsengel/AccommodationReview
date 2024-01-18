b5ac5332d4a7bd5ceaadbf647798d0f5cd9614df
a22angelgp <a22angelgp@iessanclemente.net>
2024-01-18 12:36:42 +0100

    (+) GUI First Steps 2

--- view/main_frame.py
+++ view/main_frame.py
@@ -5,26 +5,26 @@ from tkinter import ttk
 def on_button_click():
     selected_country = combo_box.get()
     user_input = entry.get()
-    result_label.config(text=f"País seleccionado: {selected_country}\nTexto ingresado: {user_input}")
+    result_label.config(text=f"Selected country : {selected_country}\nSent info: {user_input}")
 
 
 # Crear la ventana principal
 ventana = tk.Tk()
-ventana.title("Acommodation Finder")
+ventana.title("Accommodation Finder")
 ventana.geometry("600x400")
 ventana.resizable(False, False)
 # color de fondo
 ventana.config(bg="black")
 
 # Crear una etiqueta
-title_label = tk.Label(ventana, text="Acommodation Finder GUI", font=("Arial", 20))
+title_label = tk.Label(ventana, text="Accommodation Finder GUI", font=("Arial", 20))
 title_label.config(fg="white", bg="black")
 title_label.pack(pady=10)
 
 # Crear un ComboBox con algunos países
 paises = ["Netherlands", "United Kingdom", "France", "Spain", "Italy", "Austria"]
 combo_box = ttk.Combobox(ventana, values=paises, state="readonly")
-combo_box.set("Selecciona un país")
+combo_box.set("¿Where are you going?")
 combo_box.pack(pady=10)
 
 # Crear un cuadro de texto
